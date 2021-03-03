239d82de28f79ba04eafabb925f2a39b5955bce2
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-03-03 15:05:56 +0100

    ex

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Pop stantot',
+  address: '10 rue de la Paix, 75008 Paris',
+  description: 'Un très bel appartement de charme situé en plein centre ville',
+  price_per_night: 500,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Quartier rouge',
+  address: 'Amsterdam',
+  description: 'A lovely neighborhood with a lot of places to hang around',
+  price_per_night: 200,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Finance district',
+  address: 'New York',
+  description: 'A nice place to steal some bandits',
+  price_per_night: 1000,
+  number_of_guests: 2
+)
